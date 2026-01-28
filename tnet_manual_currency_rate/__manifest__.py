017537059712ecebeb0e73692428f64b4bfcf0c5
pjabie <pjabie@gauchocode.com>
2026-01-28 14:07:08 -0300

    UPD

--- tnet_manual_currency_rate/__manifest__.py
+++ tnet_manual_currency_rate/__manifest__.py
@@ -8,7 +8,7 @@
     'author': 'Luis Trajtenberg',
     'website': 'https://www.tecnicanet.com',
     'category': "Localization/Argentina",
-    'depends': ['base', 'sale', 'purchase', 'l10n_ar', 'l10n_ar_ux', 'account_ux'],
+    'depends': ['base', 'sale', 'purchase', 'l10n_ar', 'l10n_ar_ux', 'account_ux','stock_picking_invoice_link'],
     'data': [
         "security/security.xml",
         "security/ir.model.access.csv",
