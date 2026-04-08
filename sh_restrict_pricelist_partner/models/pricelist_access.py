6e8c7477db851a1fef5c0d93fb32a60b8b3e0cfb
pjabie <pjabie@gauchocode.com>
2026-04-07 23:12:21 -0300

    fix(sh_restrict_pricelist_partner): migrar _search a v18 (quitar count/access_rights_uid)

--- sh_restrict_pricelist_partner/models/pricelist_access.py
+++ sh_restrict_pricelist_partner/models/pricelist_access.py
@@ -17,32 +17,19 @@ class SalePricelist(models.Model):
     partner_pricelist_ids = fields.Many2many(
         "product.pricelist", compute='_compute_partner_pricelist_ids')
 
-    pricelist_id = fields.Many2one(
-        "product.pricelist", string='Pricelist', check_company=True,  # Unrequired company
-        required=True, readonly=True, states={'draft': [('readonly', False)], 'sent': [('readonly', False)]},
-        domain="[('id', 'in', partner_pricelist_ids),'|',('company_id', '=', False), ('company_id', '=', company_id)]",
-        help="If you change the pricelist, only newly added lines will be affected.")
-
     @api.depends('partner_id')
     def _compute_partner_pricelist_ids(self):
         for rec in self:
             if rec.partner_id and rec.partner_id.pricelist_ids:
                 rec.partner_pricelist_ids = rec.partner_id.pricelist_ids
-
             else:
-                rec.partner_pricelist_ids = self.env['product.pricelist'].search([
-                ])
-                
+                rec.partner_pricelist_ids = self.env['product.pricelist'].search([])
+
     @api.onchange('partner_id')
     def onchange_partner_id(self):
         res = super(SalePricelist, self).onchange_partner_id()
         values = {}
         self.pricelist_id = False
-        if 'website_id' in self.env.context:
-            company = self.env.company.id
-            res = self.env['product.pricelist']._get_partner_pricelist_multi(self.ids, company_id=company)
-            for p in self:
-                p.pricelist_id = res.get(p.id)
         if self.partner_pricelist_ids:
             for pr_list in self.partner_id.pricelist_ids:
                 values['pricelist_id'] = (pr_list.id if pr_list else False)
@@ -54,9 +41,8 @@ class PricelistInherit(models.Model):
     _inherit = 'product.pricelist'
 
     @api.model
-    def _search(self, args, offset=0, limit=None, order=None, count=False, access_rights_uid=None):
+    def _search(self, domain, offset=0, limit=None, order=None):
+        # v18: firma sin count ni access_rights_uid
         if self.env.user.has_group('base.group_portal') and self.env.user.partner_id.pricelist_ids.ids:
-            args.append(('id', 'in', self.env.user.partner_id.pricelist_ids.ids))
-        res = super(PricelistInherit, self)._search(args, offset=offset, limit=limit,
-                                                    order=order, count=count, access_rights_uid=access_rights_uid)
-        return res
+            domain = list(domain) + [('id', 'in', self.env.user.partner_id.pricelist_ids.ids)]
+        return super()._search(domain, offset=offset, limit=limit, order=order)
